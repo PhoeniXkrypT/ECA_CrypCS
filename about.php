f79ae1bb1110d39863c35e775a5f4febbabf9469
archanabs <archanabs15@gmail.com>
2013-11-29 15:21:38 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+ <link href="site.css" rel="stylesheet">
+<head>
+  <title>About</title>
+</head>
+
+<body>
+<h1>About Us</h1>
+<p>The site is made as a part of the S1 Mtech CSN 2013-2015 Cryptography Case Study by Group3.</p>
+<p>The aim is to create a website for implementing Enterprise Certificate Authority.</p>
+
+<form action="home.php" method="post">
+<input type="submit" name="submit" value="HOME">
+</body>
+
+</html>
