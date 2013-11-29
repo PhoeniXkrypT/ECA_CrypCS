5cf69e5233c6ef13cfb4997afd3210d80931b370
archanabs <archanabs15@gmail.com>
2013-11-29 15:22:51 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php include("header.php"); ?>
+<html>
+<head>
+  <title>ECA</title>
+  <link href="site.css" rel="stylesheet">
+</head>
+
+<body>
+  <div id="main">
+  <h1>Welcome to ECA !!!</h1>
+  <h2> Enterprise Certificate Authority</h2>
+
+  <p>Helps you to get a digital certificate.</p>
+
+<?php include("footer.php"); ?>
+
+</body>
+</html> 
